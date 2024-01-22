fbb9ea97d72de62bf45eef5086f37e95542b1bc4
SandraSagar <sandrasagar0802@gmail.com>
2024-01-22 23:38:19 +0530

    add a java file

---
+++ calculator.java
@@ -0,0 +1,12 @@
+public class calculator {
+public static void main(String[] args){
+int num1=5;
+int num2=4;
+int sum= num1+num2;
+int diff=num1-num2;
+int product=num1*num2;
+System.out.println("sum = "+sum);
+System.out.println("difference = "+diff);
+System.out.println("product = "+product);
+}
+}
--- calculator.py
+++
@@ -1,7 +0,0 @@
-num1=5
-num2=5
-print("sum = ",num1+num2)
-print("difference = ",num1-num2)
-print("product = ",num1*num2)
-print("qoutient = ",num1/num2)
-print(" modulous = ",num1%num2)
